a96b3f3ba2377c0b90254deb468d1cc78f256c1a
charlotteblassel <charlotte.blassel@hetic.net>
2021-02-08 15:42:46 +0100

    added search screen

--- screen/Search.js
+++ screen/Search.js
@@ -2,9 +2,4 @@ import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function Search(props) {
-  return (
-    <View>
-      <Text>Search</Text>
-    </View>
-  );
 }
